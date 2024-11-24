7afc39750af3d67c59cb23c53df9eaaceb8dc0c5
npc-katya <k.nekrasova.k@gmail.com>
2024-11-24 20:39:43 +0400

    feat: LAB4-3 Добавление ранне созданных страниц в проект

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './css/styles.css';
+import './css/error.css';
+import './css/main.css';
+import './css/media.css';
+import './css/reset.css';
+
+import logo from './img/logo1.png';
+
+import myFunc from './js/main.js';
+
+import i404 from './img/404 error.svg'; 
+import navclose from './img/nav-close.svg';
+import navopen from './img/nav-open.svg';
+
+
+import r1 from './img/space.png'; 
